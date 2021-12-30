1f8aee89533dd203aac9b02e9dfe79fcf011643f
Evan Suslovich <evan.suslovich@lincolnps.org>
2021-12-30 15:22:21 -0500

    comm

--- src/componenets/topbar/Topbar.jsx
+++
@@ -1,9 +0,0 @@
-import "./topbar.scss"
-
-export default function Topbar() {
-    return (
-        <div classname="topbar">
-        
-        </div>
-    )
-}
---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,9 @@
+import "./topbar.scss"
+
+export default function Topbar() {
+    return (
+        <div classname="topbar">
+        hi
+        </div>
+    )
+}
