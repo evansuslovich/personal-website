842a4dc9f0c2dacbee9767c0b69b89f186300c3d
Evan Suslovich <evan.suslovich@lincolnps.org>
2021-12-31 02:23:12 -0500

    finished left side of topbar. fixed bug with icons and will work on why the topbar slides

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,8 +1,8 @@
 import "./topbar.scss"
 
 
-
-
+import BoyIcon from '@mui/icons-material/Boy';
+import EmailIcon from '@mui/icons-material/Email';
 
 export default function Topbar() {
     return (
@@ -11,7 +11,12 @@ export default function Topbar() {
                 <div className="left"> 
                     <a href="#intro" className = "logo" >Genius. </a>
                     <div className="itemContainer">
-                    
+                        <BoyIcon className = "icon" />
+                        <span>(401)-428-4140</span>
+                    </div>
+                    <div className="itemContainer">
+                        <EmailIcon className="icon"/>
+                        <span>suslovich.e@northeastern.edu</span>
                     </div>
                 </div>
                 <div className="right"> 
