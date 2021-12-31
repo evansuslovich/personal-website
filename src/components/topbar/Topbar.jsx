73672592f046755075c57cc175cfc75817b73ea3
Evan Suslovich <evan.suslovich@lincolnps.org>
2021-12-30 19:43:35 -0500

    more topbar menu work

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -4,8 +4,19 @@ export default function Topbar() {
     return (
         <div className="topbar">
             <div className="wrapper">
+                <div className="left"> 
+                <a href="#intro"> className = "logo</a>
                 
+                </div>
+           
+                
+
+                <div className="right"> 
+                this is right
+                </div>
+
             </div>
         </div>
     )
 }
+
