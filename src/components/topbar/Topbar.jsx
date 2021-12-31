63cda24c2a71924d6b92b58320709350b2d6b7ec
Evan Suslovich <evan.suslovich@lincolnps.org>
2021-12-31 01:37:58 -0500

    working with material ui icons

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,16 +1,18 @@
 import "./topbar.scss"
+import BoyIcon from '@mui/icons-material/Boy';
+import EmailIcon from '@mui/icons-material/Email';
 
 export default function Topbar() {
     return (
         <div className="topbar">
             <div className="wrapper">
                 <div className="left"> 
-                <a href="#intro"> className = "logo</a>
-                
+                    <a href="#intro" className = "logo" >Genius. </a>
+                    <div className="itemContainer">
+                    <BoyIcon/>
+                    <EmailIcon/>
+                    </div>
                 </div>
-           
-                
-
                 <div className="right"> 
                 this is right
                 </div>
