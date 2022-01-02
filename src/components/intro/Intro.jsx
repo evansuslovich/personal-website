d410139fee2d64ae4198c0b099d1b41c5f3e1700
Evan Suslovich <evan.suslovich@lincolnps.org>
2022-01-02 17:44:06 -0500

    added assets (images) that i will use. working on my profile picture

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -3,6 +3,12 @@ import "./intro.scss"
 export default function Intro() {
     return (
         <div className="intro" id="intro">
+            <div className="left">
+                <div className="imgContainer">
+                    <img src="assets/evan-suslovich.png" alt="" />
+                </div>
+            </div>
+            <div className="right"></div>
 
         </div>
     )
