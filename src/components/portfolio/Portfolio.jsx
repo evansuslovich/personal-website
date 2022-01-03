abfc71b58193e0206dad82f3000d9b6147c1fc46
Evan Suslovich <evan.suslovich@lincolnps.org>
2022-01-03 17:41:47 -0500

    working on the portfolio

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,6 +3,19 @@ import "./portfolio.scss"
 export default function Portfolio() {
     return (
         <div className="portfolio" id="portfolio">
+            <h1>Portfolio</h1>
+            <ul>
+                <li>Projects</li>
+                <li>Art</li>
+                <li>Club</li>
+                <li>Writing</li>
+            </ul>
+
+            <div className="container">
+                <div className="item"></div>
+                <img src="" alt="" />
+                <h3></h3>
+            </div>
 
         </div>
     )
