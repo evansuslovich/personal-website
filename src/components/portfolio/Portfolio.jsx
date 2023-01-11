2e16f642e02c033427c382018ad4b0eb0ebd7b93
Evan Suslovich <evanjacobsuslovich@gmail.com>
2023-01-11 11:22:34 -0500

    adding repos

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,49 +1,78 @@
-import "./portfolio.scss"
-
-import { useEffect } from "react"
+import { useEffect, useState } from "react";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
 
 export default function Portfolio() {
+    const [avatarURL, setAvatarURL] = useState();
+    const [githubUsername, setGitHubUsername] = useState();
+    const [repoData, setRepoData] = useState();
 
+    async function repoDataURL() {
+        //Get repo data about github user
+        await fetch("https://api.github.com/users/evansuslovich/repos")
+            .then((res) => res.json())
+            .then(
+                (result) => {
+                    const list = result.map((item) => (
+                        <div className="text-center">
+                            <a href={item.svn_url}>
+                                {item.name}
+                            </a>
+                        </div>
+                    ));
+                    setRepoData(list);
+                },
+                (error) => {
+                    console.log(error);
+                }
+            );
+    }
 
-    useEffect( () => {
+    useEffect(() => {
+        fetch("https://api.github.com/users/evansuslovich")
+            .then((res) => res.json())
+            .then(
+                (result) => {
+                    console.log(result);
+                    setAvatarURL(result.avatar_url);
+                    setGitHubUsername(result.login);
+                },
+                (error) => {
+                    console.log(error);
+                }
+            );
         fetch("https://api.github.com/users/evansuslovich/repos")
-        .then((res) => res.json())
-        .then(
-            (result) => {
-                console.log(result)
-            },
-            (error) => {
-                console.log(error)
-            }
-        )
-    }, )
-
-    // useEffect(() => {
-    //     fetch("https://api.github.com/users/evansuslovich")
-    //         .then((res) => res.json())
-    //         .then(
-    //             (result) => {
-    //                 console.log(result);
-    //             },
-    //             (error) => {
-    //                 console.log(error);
-    //             }
-    //         );
-    // }, []);
+            .then((res) => res.json())
+            .then(
+                (result) => {
+                    const list = result.map((item) => (
+                        <div className="text-center">
+                            <a href={item.svn_url}>
+                                {item.name}
+                            </a>
+                        </div>
+                    ));
+                    setRepoData(list);
+                },
+                (error) => {
+                    console.log(error);
+                }
+            );
 
 
+
+    }, []);
     return (
-        <div className="portfolio" id="portfolio">
-            <h1>Portfolio</h1>
-            <ul>
-                <li>Projects</li>
-                <li>Art</li>
-                <li>Club</li>
-                <li>Writing</li>
-            </ul>
+        <div className="App w-100 min-vh-100 justify-content-center align-items-center d-flex flex-column">
+            <Card style={{ width: "18rem" }}>
+                <Card.Img variant="top" src={avatarURL} />
+                <Card.Body>
+                    <Card.Title>{githubUsername}</Card.Title>
 
-          
-
+                   {repoData}
+                </Card.Body>
+            </Card>
         </div>
-    )
+    );
 }
+
