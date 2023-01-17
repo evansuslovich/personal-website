0e2ea1ebcbeaf878feebf56055243703ce0fbef4
Evan Suslovich <evanjacobsuslovich@gmail.com>
2023-01-17 13:03:12 -0500

    put the intro slide back in and chagned the sizing of the text .. no need for a picture too tacky

--- src/App.jsx
+++ src/App.jsx
@@ -12,22 +12,19 @@ import "./app.scss";
 import {useState} from "react";
 
 function App() {
-  const[menuOpen,setMenuOpen] = useState(false)
+  const[menuOpen, setMenuOpen] = useState(false)
+
   return (
     <div className="app">
     <Topbar menuOpen = {menuOpen} setMenuOpen = {setMenuOpen}/>
     <Menu menuOpen = {menuOpen} setMenuOpen = {setMenuOpen}/>
       <div className="sections">
-        {/* <Intro/> */}
+        <Intro/>
         <Portfolio/>
         <Works/>
         <Testimonials/>
         <Contact/>
-      
-
       </div>
-   
-
     </div>
   );
 }
