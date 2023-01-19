c7186d19b5e2c7266c75be4372872acffc08cb7a
Evan Suslovich <evanjacobsuslovich@gmail.com>
2023-01-19 01:17:15 -0500

    cleaning

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,17 +1,17 @@
 import "./intro.scss";
 import { init } from 'ityped'
-import { useEffect, useRef} from "react";
+import { useEffect, useRef } from "react";
 
 export default function Intro() {
     const textRef = useRef();
     useEffect(() => {
-        init(textRef.current, { 
+        init(textRef.current, {
             showCursor: false,
             backDelay: 1000,
             strings: ["Person", "Developer", "Designer", "Painter", "Student"],
         });
 
-    
+
     }, []);
     return (
         <div className="intro" id="intro">
@@ -21,11 +21,12 @@ export default function Intro() {
                     <h1>Evan Suslovich</h1>
                     <h3>A <span ref={textRef}> </span></h3>
                 </div>
-                
+
                 <a href="#portfolio">
                     <img src="assets/down.png" alt="" />
                 </a>
-                    
+
+
             </div>
 
         </div>
