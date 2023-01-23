ba7e786eb8ff60f80c1e8f6f5e02f4f5a265054c
Evan Suslovich <evanjacobsuslovich@gmail.com>
2023-01-22 19:27:51 -0500

    npm

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,33 +1,33 @@
 import "./contact.scss"
 import React, { useRef } from 'react';
+import { useSnackbar } from 'notistack';
 
 import emailjs from '@emailjs/browser';
 
 
 export default function Contact() {
     const form = useRef();
+    const { enqueueSnackbar } = useSnackbar();
 
     const sendEmail = (e) => {
         e.preventDefault();
 
         emailjs.sendForm('service_37xv1ye', 'template_ong9vze', form.current, 'YYMYljATc_h9nzCRv')
             .then((result) => {
-                console.log(result.text);
+                enqueueSnackbar('Email Sent', { variant: 'success' });
             }, (error) => {
-                console.log(error.text);
+                enqueueSnackbar(error.text, { variant: 'error' });
             });
     };
 
     return (
         <div className="contact" id="contact">
 
-
             <h1>Contact me!</h1>
-
 
             <div className="contact-me">
 
-                <form ref={form} className="form">
+                <form ref={form} onSubmit={sendEmail} className="form">
 
                     <input className="name" type="text" name="user_name" placeholder="First Name" />
                     <input className="name" type="text" name="last_name" placeholder="Last Name" />
@@ -35,6 +35,7 @@ export default function Contact() {
                     <input className="personal" type="text" name="subject" placeholder="Subject" />
                     <textarea className="message" type="text" name="message" placeholder="Message" />
                     <input className="button" type="submit" value="Send" />
+
                 </form>
             </div>
         </div>
