e50871894df10e45abbe5bc90dc45d13dd11507f
Evan Suslovich <evanjacobsuslovich@gmail.com>
2023-01-22 23:00:23 -0500

    intro to github repos

--- src/App.jsx
+++ src/App.jsx
@@ -2,26 +2,28 @@
 import Contact from './components/contact/Contact';
 import Intro from './components/intro/Intro';
 import Portfolio from './components/portfolio/Portfolio';
+import PortfolioIntro from './components/portfolio/PortfolioIntro';
 import Topbar from './components/topbar/Topbar'
 import Works from './components/works/Works';
 import Menu from './components/menu/Menu';
 
 import "./app.scss";
 
-import {useState} from "react";
+import { useState } from "react";
 
 function App() {
-  const[menuOpen, setMenuOpen] = useState(false)
+  const [menuOpen, setMenuOpen] = useState(false)
 
   return (
     <div className="app">
-    <Topbar menuOpen = {menuOpen} setMenuOpen = {setMenuOpen}/>
-    <Menu menuOpen = {menuOpen} setMenuOpen = {setMenuOpen}/>
+      <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
       <div className="sections">
-        <Intro/>
-        <Portfolio/>
-        <Works/>
-        <Contact/>
+        <Intro />
+        <PortfolioIntro />
+        <Portfolio />
+        <Works />
+        <Contact />
       </div>
     </div>
   );
--- src/components/portfolio/PortfolioIntro.jsx
+++ src/components/portfolio/PortfolioIntro.jsx
@@ -1,9 +1,26 @@
 import React from "react";
+import { init } from 'ityped'
+import { useEffect, useRef } from "react";
+
+import "./portfolio-intro.scss"
 
 export default function PortfolioIntro() {
 
+  const textRef = useRef();
+  useEffect(() => {
+    init(textRef.current, {
+      showCursor: false,
+      backDelay: 500,
+      strings: ["Check out my GitHub Repositories!"],
+    });
+
+
+  }, []);
+
   return (
-    <div className="PortfolioIntro">
+    <div className="portfolio-intro">
+
+      <h1 ref={textRef}> </h1>
 
     </div>
   )
