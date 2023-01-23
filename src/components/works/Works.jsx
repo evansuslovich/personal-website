da52a642612435372457cd264df595da048369d7
Evan Suslovich <evanjacobsuslovich@gmail.com>
2023-01-23 16:00:15 -0500

    added paitnings and the cards to organize them

--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -7,15 +7,31 @@ export default function Works() {
             <h1>Paintings!</h1>
 
             <div className="paintings">
-                <img src="paintings/art_1.jpeg" className="painting" alt="" />
-                <img src="paintings/art_2.jpeg" className="painting" alt="" />
-                <img src="paintings/art_3.jpg" className="painting" alt="" />
-                <img src="paintings/art_4.jpeg" className="painting" alt="" />
-                <img src="paintings/art_5.jpeg" className="painting" alt="" />
-                <img src="paintings/art_6.jpeg" className="painting" alt="" />
-                <img src="paintings/art_7.jpeg" className="painting" alt="" />
+
+                <div className="painting-card">
+                    <img src="paintings/art_1.jpeg" className="painting" alt="" />
+                </div>
+                <div className="painting-card">
+                    <img src="paintings/art_2.jpeg" className="painting" alt="" />
+                </div>
+                <div className="painting-card">
+                    <img src="paintings/art_3.jpg" className="painting" alt="" />
+                </div>
+                <div className="painting-card">
+                    <img src="paintings/art_4.jpeg" className="painting" alt="" />
+                </div>
+                <div className="painting-card">
+                    <img src="paintings/art_5.jpeg" className="painting" alt="" />
+                </div>
+                <div className="painting-card">
+                    <img src="paintings/art_6.jpeg" className="painting" alt="" />
+                </div>
+                <div className="painting-card">
+                    <img src="paintings/art_7.jpeg" className="painting" alt="" />
+                </div>
+                
             </div>
 
-        </div>
+        </div >
     )
 }
