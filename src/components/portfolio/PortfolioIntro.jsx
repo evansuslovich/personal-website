f121a99aaf59b6d0d1986ea56d823e40a5dece0f
Evan Suslovich <evanjacobsuslovich@gmail.com>
2023-01-23 11:18:25 -0500

    adding portfoliointro into portfolio

--- src/components/portfolio/PortfolioIntro.jsx
+++ src/components/portfolio/PortfolioIntro.jsx
@@ -1,27 +1,12 @@
-import React from "react";
-import { init } from 'ityped'
-import { useEffect, useRef } from "react";
-
 import "./portfolio-intro.scss"
 
 export default function PortfolioIntro() {
 
-  const textRef = useRef();
-  useEffect(() => {
-    init(textRef.current, {
-      showCursor: false,
-      backDelay: 500,
-      strings: ["Check out my GitHub Repositories!"],
-    });
-
-
-  }, []);
-
   return (
     <div className="portfolio-intro">
-
-      <h1 ref={textRef}> </h1>
-
+      <div className="text">
+        <h1>My Personal Projects!</h1>
+      </div>
     </div>
   )
 
