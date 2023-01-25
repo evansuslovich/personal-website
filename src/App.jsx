c42162af36782fcb602d470212417583c974d1ed
Evan Suslovich <63474660+evansuslovich@users.noreply.github.com>
2023-01-25 11:14:14 -0500

    Update App.jsx
    uncomment Intro

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
     <Topbar menuOpen = {menuOpen} setMenuOpen = {setMenuOpen}/>
     <Menu menuOpen = {menuOpen} setMenuOpen = {setMenuOpen}/>
       <div className="sections">
-        {/* <Intro/> */}
+        <Intro/>
         <Portfolio/>
         <Works/>
         <Contact/>
