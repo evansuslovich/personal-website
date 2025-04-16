bd4e44e5726492a392d989c18d60b9d3b4aca3f6
evansuslovich <evanjacobsuslovich@gmail.com>
2025-04-16 11:42:19 -0400

    updated

--- src/components/resume/Resume.jsx
+++ src/components/resume/Resume.jsx
@@ -153,12 +153,12 @@ export default function Resume() {
                 startDate="January 2024"
                 endDate="June 2024"
                 details={[
-                  <Bullet point="Aligned React UI with Figma board, improved data presentation, and refactored Typescript files for code quality" />,
-                  <Bullet point="Optimized biomedical metadata queries to GCP and Microsoft Azure via Java Spring Boot" />,
-                  <Bullet point="Iterated on the Spring Boot backend to optimize biomedical metadata queries hosted on Google Cloud and Azure" />,
+                  <Bullet point="Aligned React UI with Figma board, improved data presentation in 5 components, refactored 22 misaligned types" />,
+                  <Bullet point="Enhanced 4 Spring Boot REST APIs by extending SQL queries to incorporate clinical data from GCP and Azure" />,
+                  <Bullet point="Refactored 7 complex constructors using the Builder pattern, enhancing code reusability in 5 API endpoints" />,
                   <Bullet point="Refactored legacy cloud query workflow to streamline and remove years of technical debt" />,
                   <Bullet point="Automated the API profiling process to facilitate data-driven performance optimization with Python scripting" />,
-                  <Bullet point="Improved custom cohort creation, data accessibility, and biomedical research workflows by closing 27 PRs" />,
+                  <Bullet point="Contributed to custom cohort creation, data accessibility, and biomedical research workflows by merging 27 PRs" />,
                 ]}
               />
               <ListItem
@@ -168,11 +168,11 @@ export default function Resume() {
                 startDate="May 2023"
                 endDate="September 2023"
                 details={[
-                  <Bullet point="Implemented Jest framework in React frontend with 287 tests across 16 functions ensuring robust code quality" />,
-                  <Bullet point="Resolved 40% of frontend crashes with a solution for undefined behavior with icons in Material-UI components" />,
-                  <Bullet point="Developed a background task for concurrent large data downloads and automated zipping and emailing" />,
-                  <Bullet point="Identified weak password validation on registration and implemented custom password validation with Django" />,
-                  <Bullet point="Created a multi-layered system heuristic for tab naming conventions enhancing 80% of multi-query searches" />,
+                  <Bullet point="Integrated Jest testing framework in React frontend with 287 tests across 16 functions, ensuring robust code quality and reliability" />,
+                  <Bullet point="Resolved 40% of frontend crashes by addressing undefined behavior in Material-UI icon rendering logic" />,
+                  <Bullet point="Engineered a background task for concurrent large data downloads with automated zipping and email delivery" />,
+                  <Bullet point="Enhanced registration security by implementing custom password validation logic in Django with frontend support" />,
+                  <Bullet point="Developed a multi-layered heuristic system for tab naming conventions, improving analytical clarity for 80% of multi-query searches" />,
                 ]}
               />
 
@@ -183,11 +183,11 @@ export default function Resume() {
                 startDate="May 2022"
                 endDate="May 2023"
                 details={[
-                  <Bullet point="Initialized full-stack application with a Django backend and React frontend" />,
-                  <Bullet point="Streamlined API development with RTK Query eliminating the need for manual data fetching and caching" />,
-                  <Bullet point="Designed proof-of-concept frontend with wireframing, Material UI, SASS, and React to handle scalability" />,
-                  <Bullet point="Implemented user authentication with Django REST, CSRF validation, and password reset with email " />,
-                  <Bullet point="Developed first version of Search feature supporting online news, Twitter, Reddit, and YouTube" />,
+                  <Bullet point="Built a full-stack application from the ground up using Django (backend) and React (frontend)" />,
+                  <Bullet point="Accelerated frontend development by integrating RTK Query, removing the need for manual data fetching and caching logic" />,
+                  <Bullet point="Designed a scalable proof-of-concept UI with wireframes, React, SASS, and Material UI for early-stage researchers" />,
+                  <Bullet point="Implemented secure user authentication with Django REST, including CSRF validation and password reset via email" />,
+                  <Bullet point="Launched the first version of the Search feature with support for online news, Twitter, Reddit, and YouTube sources" />,
                 ]}
               />
             </Section>
@@ -199,7 +199,7 @@ export default function Resume() {
                 endDate="January 2025"
                 details={[
                   <Bullet point="Implemented Advantage Actor-Critic (A2C) and Soft Actor-Critic (SAC) algorithms to train and compete chess agents" />,
-                  <Bullet point="Evaluated performance based on win rates, demonstrating SAC achieving a 4.1:1 win-to-loss ratio over A2C" />,
+                  <Bullet point="Evaluated performance based on win rates, demonstrated SAC achieving a 4.1:1 win-to-loss ratio over A2C" />,
                   <Bullet point="Conducted comparative analysis of both algorithms, highlighting strengths and weaknesses in decision-making" />,
                 ]}
               />
@@ -210,7 +210,7 @@ export default function Resume() {
                 details={[
                   <Bullet point="Achieved nearly 80% accuracy in artist classification with a CNN model, making AI-driven art analysis accessible" />,
                   <Bullet point="Developed a React application to visualize model accuracy, adjust hyperparameters, and explore real-time classification" />,
-                  <Bullet point="Built a scalable Flask backend for model creation, training, and data storage for future expansions and dataset updates" />,
+                  <Bullet point="Built a scalable Flask backend to support model training, versioned data storage, and future dataset expansion" />,
                 ]}
               />
             </Section>
